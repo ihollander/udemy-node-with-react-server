4e8b81bf185188a7e4d5ca2c263a0093f04fb3c7
Ian Hollander <ianholla@gmail.com>
2019-03-15 11:43:16 -0400

    added mongoose

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport");
 
+mongoose.connect(keys.mongoUri, { useNewUrlParser: true }).catch(console.error);
 const app = express();
 
 require("./routes/authRoutes")(app);
