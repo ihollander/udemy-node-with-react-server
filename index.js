5bdbf404247a18e215e09d70131293bb03678d27
ihollander <ianholla@gmail.com>
2019-03-15 06:50:25 -0400

    gitignore fix;

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+require("dotenv").load();
+
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-const keys = require("./config/keys");
+// const keys = require("./config/keys");
 
 const app = express();
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
